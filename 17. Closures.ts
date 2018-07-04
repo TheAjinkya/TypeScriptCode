372309692a7c7d851f39dd8e8b7684b45eba4e1e
Theajinkya <ajinkya.chanshetty@gmail.com>
2018-07-04 23:59:13 +0530

    Closures

---
+++ 17. Closures.ts
@@ -0,0 +1,33 @@
+//This is the Simplest form of a closure. Because the variables follow the lexical scopes and global functions are accessible inside every other function
+
+function addTo() : number{
+
+var inner = 5;
+
+return inner + passed
+
+}
+
+var passed = 10;
+
+let result = addTo();
+
+console.log("The result is: "+ result)
+
+
+var sub = function(passed){
+
+var subInner = 100;
+
+var subTo = function(subInner){
+
+return subInner + passed
+
+}
+
+return subTo
+
+}
+
+console.log(sub)
+
